a9ad4f3363c180e6c189d3e4924ebac1623f2bdf
oddbookworm <andrewryancoffey@hotmail.com>
2026-07-25 09:44:07 -0500

    Refactored logger to be single-header and compile-time log level definition

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,55 +1,21 @@
-#include "Logger.h"
-#include "ConsoleSink.h"
-#include "RotatingFileSink.h"
-#include <thread>
-#include <string.h>
-#include <math.h>
+#include "ErrorLogger.h"
 
-std::mutex _startLogging;
+using namespace Nucleus;
 
-int randi(int lo, int hi)
+void task(const std::string& message)
 {
-    int n = hi - lo + 1;
-    int i = rand() % n;
-    if (i < 0) i = -i;
-    return lo + i;
-}
-
-void task(const std::string& message, int delay)
-{
-    auto& logger = Logger::instance();
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(delay));
-
-    std::lock_guard<std::mutex> lock(_startLogging);
-
-    logger.log(LogLevel::ERROR, message);
+    WARN(message);
 }
 
 int main() {
-    auto& logger = Logger::instance();
+    TRACE("Program started.");
 
-    logger.addSink(std::make_shared<ConsoleSink>());
-    logger.addSink(std::make_shared<RotatingFileSink>("app.log"));
-
-    logger.log(LogLevel::INFO, "Program started with rotating file logger.");
-
-    _startLogging.lock();
-
-    std::vector<std::thread> _threads;
-    const std::string messageTemplate = "Logging On Thread: ";
+    const std::string messageTemplate = "Logging message ";
     for (uint32_t idx = 0U; idx < 1'000U; ++idx)
     {
         char message[30] = "";
         sprintf(message, "%s%u", messageTemplate.c_str(), idx);
-        _threads.emplace_back(std::thread(task, message, randi(0, 100)));
-    }
-
-    _startLogging.unlock();
-
-    for (auto& th : _threads)
-    {
-        th.join();
+        task(message);
     }
 
     return 0;
